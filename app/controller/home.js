92f60a13077315b273c83fef2d8bd945bf107b8e
lijialiang93 <lijialiang93@gmail.com>
2018-08-06 16:06:52 +0800

    change

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'testing...';
+    this.ctx.body = 'testing...!!';
   }
 }
 
