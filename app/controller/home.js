516b01448eb46674b4d2c2e57d4d69ec0b68e1ec
lijialiang93 <lijialiang93@gmail.com>
2018-08-06 17:00:29 +0800

    aaa

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'Just pushed this message from local computer...'
+    this.ctx.body = 'Just pushed this message...'
   }
 }
 
